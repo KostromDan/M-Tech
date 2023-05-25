5d34fe2d1de19e87834de88b54cee63cdc43e46b
Даниил <KostromDan@gmail.com>
2023-05-25 05:31:57 +0300

    1.0.4

---
+++ overrides/kubejs/server_scripts/CW/CW.js
@@ -0,0 +1,6 @@
+ServerEvents.recipes(event => {
+    event.replaceInput({mod: 'constructionwand'}, 'minecraft:diamond_block', 'mekanism:meka_tool')
+    event.replaceInput({mod: 'constructionwand'}, 'minecraft:nether_star', 'create:mechanical_arm')
+
+
+})
